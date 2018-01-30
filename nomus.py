5f69f7e85903e55d28dc54090159d887c132047f
Vasilii-Moskvin <vasiliy.moscvin@yandex.ru>
2018-01-30 12:10:22 +0300

    Renamed some bad names for variables.

--- nomus.py
+++ nomus.py
@@ -12,13 +12,13 @@ def get_XMatch_NOMAD_USNO(save_dir_path, ra, de, radius="14'"):
     :param radius: radius of area for catalogue
     :return: path to XMatch-file from Aladin
     """
-    XMatch_from_Aladin_path = os.path.join(save_dir_path, 'nomus_cat.csv')
+    XMatch_NOMAD_USNO_path = os.path.join(save_dir_path, 'nomus_cat.csv')
     script_for_Aladin = ['#AJS',
                          'sync',
                          'get VizieR(NOMAD1) {} {} {}'.format(ra, de, radius),
                          'get VizieR(USNO-B1) {} {} {}'.format(ra, de, radius),
                          'xmatch NOMAD1 USNO-B1 1.0',
-                         'export XMatch {}'.format(XMatch_from_Aladin_path),
+                         'export XMatch {}'.format(XMatch_NOMAD_USNO_path),
                          'quit']
 
     script_for_Aladin_path = os.path.join(save_dir_path, 'script_for_Aladin.txt')
@@ -29,17 +29,17 @@ def get_XMatch_NOMAD_USNO(save_dir_path, ra, de, radius="14'"):
     os.system('java -jar C:\Aladin.jar -script < {}'.format(script_for_Aladin_path))
     os.remove(script_for_Aladin_path)
 
-    if os.path.exists(XMatch_from_Aladin_path):
-        return XMatch_from_Aladin_path
+    if os.path.exists(XMatch_NOMAD_USNO_path):
+        return XMatch_NOMAD_USNO_path
     else:
         return False
 
 
-def convert_XMatch_to_catalogue(catalogue_from_Aladin_path):
+def convert_XMatch_to_catalogue(XMatch_NOMAD_USNO_path):
     """
     Converts XMatch-file from Aladin to csv- catalogue.
-    :param catalogue_from_Aladin_path: path to catalogue from Aladin
-    :return: converted XMatch-file to csv- catalogue
+    :param NOMAD_USNO_path: path to XMatch NOMAD1 and USNO-B1 catalogues
+    :return: csv- catalogue
     """
     convert_header = OrderedDict((('_RAJ2000_tab1', 'RAJ2000'),
                                   ('_DEJ2000_tab1', 'DEJ2000'),
@@ -49,7 +49,7 @@ def convert_XMatch_to_catalogue(catalogue_from_Aladin_path):
                                   ('Imag_tab2', 'I_mag')))
     Stars = namedtuple('Stars', convert_header.values())
 
-    with open(catalogue_from_Aladin_path, 'r') as csv_file:
+    with open(XMatch_NOMAD_USNO_path, 'r') as csv_file:
         csv_file = csv.DictReader(csv_file, delimiter='\t')
         next(csv_file)
         stars_from_file = [Stars(**dict(map(lambda key: (convert_header[key], star[key]) if star[key] != ' ' else
@@ -57,13 +57,13 @@ def convert_XMatch_to_catalogue(catalogue_from_Aladin_path):
                                             filter(lambda x: x in convert_header, star.keys()))))
                            for star in csv_file]
 
-    with open(catalogue_from_Aladin_path, 'w', newline='') as csv_file:
+    with open(XMatch_NOMAD_USNO_path, 'w', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow(convert_header.values())
         writer.writerows(stars_from_file)
 
 
-def calc_today_coordinates():
+def calc_today_coordinates(NOMAD_USNO_path, epoch):
     pass
 
 
@@ -75,9 +75,9 @@ def main():
     radius = input('Enter radius:\n')
 
     try:
-        XMatch_NOMAD_USNO = get_XMatch_NOMAD_USNO(save_dir_path, ra, de, radius)
-        if XMatch_NOMAD_USNO:
-            convert_XMatch_to_catalogue(XMatch_NOMAD_USNO)
+        XMatch_NOMAD_USNO_path = get_XMatch_NOMAD_USNO(save_dir_path, ra, de, radius)
+        if XMatch_NOMAD_USNO_path:
+            convert_XMatch_to_catalogue(XMatch_NOMAD_USNO_path)
     except OSError as e:
         print(e)
 
