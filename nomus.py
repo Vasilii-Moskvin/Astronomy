ea685394492d3747c1acc920cedf7dbee5143216
Vasilii-Moskvin <vasiliy.moscvin@yandex.ru>
2018-01-30 11:52:00 +0300

    Replaced name from_Aladin_path то NOMAD_USNO Started development function calc_today_coordinates(*args, **kwargs)

--- nomus.py
+++ nomus.py
@@ -3,7 +3,7 @@ import csv
 import os.path
 
 
-def get_XMatch_form_Aladin(save_dir_path, ra, de, radius="14'"):
+def get_XMatch_NOMAD_USNO(save_dir_path, ra, de, radius="14'"):
     """
     Gets XMatch-file from Aladin.
     :param save_dir_path: path to save directory for catalogue from Aladin
@@ -24,7 +24,7 @@ def get_XMatch_form_Aladin(save_dir_path, ra, de, radius="14'"):
     script_for_Aladin_path = os.path.join(save_dir_path, 'script_for_Aladin.txt')
     with open(script_for_Aladin_path, 'w') as w:
         for line in script_for_Aladin:
-            f.write('{}\n'.format(line))
+            w.write('{}\n'.format(line))
 
     os.system('java -jar C:\Aladin.jar -script < {}'.format(script_for_Aladin_path))
     os.remove(script_for_Aladin_path)
@@ -62,17 +62,21 @@ def convert_XMatch_to_catalogue(catalogue_from_Aladin_path):
         writer.writerow(convert_header.values())
         writer.writerows(stars_from_file)
 
+def calc_today_coordinates():
+    pass
+
 
 def main():
     save_dir_path = os.path.abspath(input('Enter the path where to save NomUs catalogue:\n'))
+    epoch = input('Enter Epoph:\n')
     ra = input('Enter RA:\n')
     de = input('Enter DE:\n')
     radius = input('Enter radius:\n')
 
     try:
-        XMatch_from_Aladin_path = get_XMatch_form_Aladin(save_dir_path, ra, de, radius)
-        if XMatch_from_Aladin_path:
-            convert_XMatch_to_catalogue(XMatch_from_Aladin_path)
+        XMatch_NOMAD_USNO = get_XMatch_NOMAD_USNO(save_dir_path, ra, de, radius)
+        if XMatch_NOMAD_USNO:
+            convert_XMatch_to_catalogue(XMatch_NOMAD_USNO)
     except OSError as e:
         print(e)
 
