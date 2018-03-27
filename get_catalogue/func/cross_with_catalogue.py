ab20e1062bdca718ca6950279d22670fa6e9bf30
Vasilii-Moskvin <vasiliy.moscvin@yandex.ru>
2018-03-27 16:49:54 +0300

    Some bugs fixed.

--- get_catalogue/func/cross_with_catalogue.py
+++ get_catalogue/func/cross_with_catalogue.py
@@ -1,22 +1,26 @@
 import os
 import re
-from func.work_with_csv import open_dict_csv, write_dict_csv
+from func.work_with_csv import open_dict_csv, write_dict_csv, get_column
 from collections import OrderedDict
 
 
 PATH_TO_ALADIN = r'/home/crao/Astronomy/Soft/Aladin/Aladin/Aladin.jar'
 
 
-def cross_with_catalogue(dir_path, cat_path):
+def cross_with_catalogue(dir_path, cat_path, two_flag):
     '''
     Reads catalogs obtained from SExtractor. Using the Aladin software produces 
     a catalog of cross-matches between the nomus SExtractor and catalogs.
     :param dir_path: path to folder with SExtracor catalogs.
-    :param cat_path: path to nomus catalogue.
+    :param cat_path: path to the reference catalogue.
+    :param two_flag: if True then all objects in the image are searched,
+                     and not only those that are in the reference directory.
     :return: saves cross-match catalogue in the folder dir_path.
     '''
     sex_files = [file.path for file in os.scandir(dir_path) if file.name.endswith('_sex.csv')]
     for sex_path in sorted(sex_files):
+        if two_flag:
+            update_catalogue(dirpath, sexpath, catpath)
         XMatchfile = get_XMatchcat_from_Aladin(dir_path, sex_path, cat_path)
         Aladin_cat_to_csv(sex_path, XMatchfile)
 
@@ -26,7 +30,7 @@ def get_XMatchcat_from_Aladin(dir_path, sex_path, cat_path):
     Using the Aladin software produces a catalog of cross-matches between the nomus SExtractor and catalogs.
     :param dir_path: path to folder with SExtracor catalogs.
     :param sex_path: path to SExtracor catalogue.
-    :param cat_path: path to nomus catalogue.
+    :param cat_path: path to the reference catalogue.
     :return: name of the cross-match catalogue in the folder dir_path. 
     '''
     bat_path = os.path.join(dir_path, 'bat.ajs')
@@ -43,7 +47,6 @@ def get_XMatchcat_from_Aladin(dir_path, sex_path, cat_path):
             f.write('{}\n'.format(line))
 
     os.system('java -jar {} -script < {}'.format(PATH_TO_ALADIN, bat_path))
-    #os.system('java -jar C:\Aladin.jar -script < {}'.format(bat_path))
     try:
         os.remove(bat_path)
     except OSError as e:
@@ -57,7 +60,7 @@ def Aladin_cat_to_csv(sex_path, XMatchfile):
     Converts the cross-match catalogue obtained from Aladin software to csv format. Removes unwanted columns.
     Deletes the catalogue obtained from SExtracor.
     :param sex_path: path to catalogue obtained from SExtracor.
-    :param cat_path: path to nomus catalogue.
+    :param cat_path: path to the reference catalogu.
     :return: saves cross-match catalogue in the folder dir_path.
     '''
     filt = re.findall(r'.*_(\w+)_sex.csv$', sex_path.split(os.sep)[-1])[0].upper()
@@ -84,3 +87,60 @@ def Aladin_cat_to_csv(sex_path, XMatchfile):
         os.remove(sex_path)
     except OSError as e:
         print(e)
+
+
+def not_match_object(sex_path, XMatchfile):
+    '''
+    Data about objects that are not in the reference catalogue.
+    :param sex_path: path to catalogue obtained from SExtracor.
+    :param XMatchfile: path to the cross-match catalogue between catalogue obtained from SExtracor and the reference catalogue.
+    :return: data about objects that are not in the reference catalogue.
+    '''
+    sex_header, sex_data = open_dict_csv(sex_path, ',')
+    match_header, match_data = open_dict_csv(XMatchfile, '\t')
+    coord = list(zip(get_column(match_data, 'ALPHA_J2000_tab2'), get_column(match_data, 'DELTA_J2000_tab2')))
+    not_match_data = list(filter(lambda x: (x['ALPHA_J2000'], x['DELTA_J2000']) not in coord, sex_data))
+
+    return not_match_data, sex_header
+
+
+def update_catalogue(dir_path, sex_path, cat_path):
+    '''
+    Adds to the reference catalogue the objects that are on the frame, but not in the reference catalogue.
+    :param dir_path: path to work folder.
+    :param sex_path: path to catalogue obtained from SExtracor.
+    :param cat_path: path to the reference catalogu.
+    :return: updated the reference catalogue. Adds to it the objects that are on the frame, but not in the reference catalogue.
+    '''
+    bat_path = os.path.join(dir_path, 'bat.ajs')
+    with open(bat_path, 'w') as f:
+        XMatchfile = '{}_XMatch.csv'.format(re.findall(r'(.+)_sex\.csv$', sex_path)[0])
+        data_to_Aladin = ['#AJS',
+                          'sync',
+                          'load {}'.format(sex_path),
+                          'load {}'.format(cat_path),
+                          'xmatch {} {} 1.0'.format(cat_path.split(os.sep)[-1], sex_path.split(os.sep)[-1]),
+                          'export XMatch {}'.format(XMatchfile),
+                          'quit']
+
+        for line in data_to_Aladin:
+            f.write('{}\n'.format(line))
+
+    os.system('java -jar /home/crao/Astronomy/Soft/Aladin/Aladin/Aladin.jar -script < {}'.format(bat_path))
+    # os.system('java -jar C:\Aladin.jar -script < {}'.format(bat_path))
+
+    try:
+        not_match_data, sex_header = not_match_object(sex_path, XMatchfile)
+        os.remove(XMatchfile)
+    except OSError as e:
+        print(e)
+
+    cat_header, cat_data = open_dict_csv(cat_path, ',')
+    for src in not_match_data:
+        temp = {'RAJ2000': src['ALPHA_J2000'], 'DEJ2000': src['DELTA_J2000']}
+        for src_1 in cat_header:
+            if src_1 not in temp:
+                temp[src_1] = '-'
+        cat_data.append(temp)
+
+    write_dict_csv(catpath, cat_header, cat_data)
