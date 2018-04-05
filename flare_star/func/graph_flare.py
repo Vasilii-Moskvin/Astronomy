2f9ede0b9a0eee9a555b570902a53a29e5f42a7f
Vasilii-Moskvin <vasiliy.moscvin@yandex.ru>
2018-04-05 21:07:54 +0300

    Added draw_LC function.

--- flare_star/func/graph_flare.py
+++ flare_star/func/graph_flare.py
@@ -25,3 +25,41 @@ def draw_aperture(xy, x_0_star, y_0_star, x_star, y_star, x_bckgrnd_in, y_bckgrn
     plt.plot(x_bckgrnd_in, y_bckgrnd_in, linestyle=' ', marker='.', markersize=1, color='green')
     plt.plot(x_bckgrnd_out, y_bckgrnd_out, linestyle=' ', marker='.', markersize=1, color='green')
     plt.plot(x_0_star, y_0_star, linestyle=' ', marker='.', markersize=3, color='blue')
+
+
+def draw_LC(time, signal, err_signal, bkgnd, err_bkgnd, delta, err_delta):#, rec_bkgnd):
+    number = 3
+    f, axarr = plt.subplots(number, sharex=True)
+    #axarr[0].plot(time, signal, linestyle=' ', marker='.', markersize=1, color='red')
+    axarr[0].errorbar(time, signal, linestyle=' ', yerr=err_signal, marker='', markersize=1, color='gray')
+    axarr[0].plot(time, signal, linestyle=' ', marker='.', markersize=1, color='red')
+    #axarr[0].plot(time, bkgnd, linestyle=' ', marker='.', markersize=1, color='green')
+    axarr[0].set_title('Light curve of full frame')
+    #set_ylim = axarr[0].get_ylim()
+    #axarr[0].set_ylim(-20, 130)
+    #axarr[0].set_ylabel('count/sec', fontsize=14)
+
+    axarr[1].errorbar(time, delta, linestyle=' ', yerr=err_delta, marker='', markersize=1, color='gray')
+    axarr[1].plot(time, delta, linestyle=' ', marker='.', markersize=1, color='blue')
+    axarr[1].set_title('Light curve of star')
+    axarr[1].set_ylabel('Intensity, count/sec', fontsize=14)
+    #axarr[1].set_ylim(set_ylim)
+
+    axarr[2].errorbar(time, bkgnd, linestyle=' ', yerr=err_bkgnd, marker='', markersize=1, color='gray')
+    axarr[2].plot(time, bkgnd, linestyle=' ', marker='.', markersize=1, color='green')
+    #axarr[2].plot(time, rec_bkgnd, linestyle=' ', marker='.', markersize=1, color='blue')
+    axarr[2].set_title('Background')
+    #axarr[2].set_ylabel('Star and background, count/sec', fontsize=14)
+    axarr[2].set_xlabel('time, s', fontsize=14)
+    #axarr[2].set_ylim(set_ylim)
+
+    # axarr[3].plot(time, list(map(lambda x, y: x / y, signal, bkgnd)), linestyle=' ', marker='.', markersize=1, color='green')
+    #axarr[3].plot(time, rec_bkgnd, linestyle=' ', marker='.', markersize=1, color='blue')
+    # axarr[3].set_title('Rate')
+    # axarr[2].set_ylabel('Star and background, count/sec', fontsize=14)
+    # axarr[3].set_xlabel('time, s', fontsize=14)
+    #axarr[3].set_ylim(set_ylim)
+
+    for index in range(number):
+        axarr[index].grid(color='grey', linestyle='--', linewidth=0.5)
+        
